29f05580869e0c392017fda60a935d13237413a6
LeandervanAarde <200211@virtualwindow.co.za>
2022-09-01 16:23:29 +0200

    Code fixed
    Code starting to be finalised

--- code.py
+++ code.py
@@ -1,164 +1,169 @@
-#place all imports here
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.mouse import Mouse
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
 import board
-import analogio
 import digitalio
 import time
+import analogio
 
-###
-# MAC OS CODE 
-###
 
-# Buttons enter here
-# Current call
+
+#upload button top right
 button1 = digitalio.DigitalInOut(board.GP0)
 button1.direction = digitalio.Direction.INPUT
 button1.pull = digitalio.Pull.UP
 
-# Deafen button
+# Code button
 button2 = digitalio.DigitalInOut(board.GP1)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
 
-# Pinned Messages Button
+# Unread Channels  Button
 button3 = digitalio.DigitalInOut(board.GP2)
 button3.direction = digitalio.Direction.INPUT
 button3.pull = digitalio.Pull.UP
 
-# Unread Channels
-button4 = digitalio.DigitalInOut(board.GP3)
+# Pinned messages Channels
+button4 = digitalio.DigitalInOut(board.GP4)
 button4.direction = digitalio.Direction.INPUT
 button4.pull = digitalio.Pull.UP
 
-#Code button 
-button5 = digitalio.DigitalInOut(board.GP4)
+#current call button
+button5 = digitalio.DigitalInOut(board.GP21)
 button5.direction = digitalio.Direction.INPUT
 button5.pull = digitalio.Pull.UP
 
-
-kbd = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(kbd)
-
-#unread inbox popout
-button6 = digitalio.DigitalInOut(board.GP5)
+button6 = digitalio.DigitalInOut(board.GP20)
 button6.direction = digitalio.Direction.INPUT
 button6.pull = digitalio.Pull.UP
 
-#Button Presses
+MuteButton = digitalio.DigitalInOut(board.GP6)
+MuteButton.direction = digitalio.Direction.INPUT
+MuteButton.pull = digitalio.Pull.UP
+muteState = False
 
-while True:
-    # Navigate to current call funtionallity
-        if not button1.value:
-            print('button two pressed')
-            kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.OPTION, Keycode.V)
-    #Deafen
-        # if not button2.value:
-        #     print('button two pressed')
-        #     kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.D)
-        
-    #Upload FIle
-        if not button2.value:
-            print('button two pressed')
-            kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.U)
-        
-        # View pinned messages (modal)
-        if not button3.value:
-            print('button three pressed')
-            kbd.send(Keycode.COMMAND, Keycode.P)
-        
-        # Navigate unread channels
-        if not button4.value: 
-            print("button 4 clicked")
-            kbd.send(Keycode.OPTION, Keycode.SHIFT, Keycode.DOWN_ARROW)
-        
-        if not button5.value:
-            print("button five pressed")
-            kbd.send(Keycode.COMMAND, Keycode.I)
-        
-        if not button6.value:
-            button6Count = 0
-            print("Button 6 has just been pressed")
-            while button6Count < 6: 
-                kbd.send(Keycode.GRAVE_ACCENT)
-                button6Count = button6Count +1
-                if button6Count == 3:
-                    kbd.send(Keycode.ENTER)
-                    kbd.send(Keycode.ENTER)
-                if button6Count == 6:
-                    kbd.send(Keycode.UP_ARROW)
-                    keyboard_layout.write("Kom ons speel RocketLeague")
-                    kbd.send(Keycode.DOWN_ARROW)
-                    kbd.send(Keycode.ENTER)
-                    kbd.send(Keycode.ENTER)
+# s0 = digitalio.DigitalInOut(board.GP16)
+# s1 = digitalio.DigitalInOut(board.GP17)
+# s2 = digitalio.DigitalInOut(board.GP18)
+# s3 = digitalio.DigitalInOut(board.GP19)
+# Sig = digitalio.DigitalInOut(board.GP28)
+# controlPin = [s0, s1, s2, s3]
 
-        time.sleep(0.3)
-        kbd.release_all()
-        
-        
-###
-# WINDOWS CODE
-###
-mouse = Mouse(usb_hid.devices)
+# muxChannel = [
+#    {0,0,0,0}, 
+#    {1,0,0,0}, 
+#    {0,1,0,0}, 
+#    {1,1,0,0}, 
+#    {0,0,1,0}, 
+#    {1,0,1,0}, 
+#    {0,1,1,0}, 
+#    {1,1,1,0}, 
+#    {0,0,0,1}, 
+#    {1,0,0,1}, 
+#    {0,1,0,1}, 
+#    {1,1,0,1}, 
+#    {0,0,1,1}, 
+#    {1,0,1,1}, 
+#    {0,1,1,1}, 
+#    {1,1,1,1}  
+# ]
+
+# Do we need to define direction and pull? 
+#Insert Deafen
+
 kbd = Keyboard(usb_hid.devices)
+mouse = Mouse(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(kbd)
+cc = ConsumerControl(usb_hid.devices)
 
-x_axis = analogio.AnalogIn(board.GP27)
-y_axis = analogio.AnalogIn(board.GP26)
+pot = analogio.AnalogIn(board.GP27)
+last_position = 0
 
-def get_value(pin):
-    return (pin.value * 3.3) / 65536
+fakeVolume = 0
 
-def steps(axis):
-    return round((axis - pot_min) / step)
+for x in range(100):
+    cc.press(ConsumerControlCode.VOLUME_DECREMENT)
 
 while True:
-    x = get_value(x_axis)
-    y = get_value(y_axis)
+    #Upload button 
+    if not button1.value:
+        print('button three pressed')
+        kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.U)
+    # Code button (Top left)
+    if not button2.value:
+        button6Count = 0
+        print("Button 6 has just been pressed")
+        while button6Count < 6:
+            kbd.send(Keycode.GRAVE_ACCENT)
+            button6Count = button6Count + 1
+            if button6Count == 3:
+                kbd.send(Keycode.ENTER)
+                kbd.send(Keycode.ENTER)
+
+            if button6Count == 6:
+                kbd.send(Keycode.UP_ARROW)
+                keyboard_layout.write("ENTER CODE IN HERE")
+                kbd.send(Keycode.DOWN_ARROW)
+                kbd.send(Keycode.ENTER)
+     #Unread channels button           
+    if not button3.value:
+        kbd.send(Keycode.OPTION, Keycode.SHIFT, Keycode.DOWN_ARROW)
+    
+    if not button4.value:
+        kbd.send(Keycode.COMMAND, Keycode.P)
+    #navigate to current call 
+    if not button5.value: 
+        print("Hello world")
+        kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.OPTION, Keycode.V)
+    #Unread messages button
+    if not button6.value:
+        kbd.send(Keycode.COMMAND, Keycode.I)
+        
+    #MUTE
+    if not MuteButton.value:
+        print(MuteButton.value)
+        if muteState:
+            kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.M)
+            kbd.release(Keycode.COMMAND, Keycode.SHIFT, Keycode.M)
+            muteState = False
+            time.sleep(1)
+    else:
+        if not muteState:
+            kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.M)
+            kbd.release(Keycode.COMMAND, Keycode.SHIFT, Keycode.M)
+            muteState = True
+            time.sleep(1)
+            
+    potVal = round((pot.value / 65536) * 50)
+
+    if (potVal % 2) == 0:
+        print ("The provided number is even")
+
+    else:
+        print ("The provided number is odd")
+        potVal = potVal - 1
 
 
-# navigate channels
-    if steps(x) > 11.0:
-        kbd.send(Keycode.ALT, Keycode.DOWN_ARROW)
-        time.sleep(0.5)        
-    if steps(x) < 9.0:
-        kbd.send(Keycode.ALT, Keycode.UP_ARROW)
-        time.sleep(0.5)   
+    print("PotVal: " ,potVal)
+    print("FakeVolume: " ,fakeVolume)
 
-    if steps(x) > 19.0:
-        kbd.send(Keycode.ALT, Keycode.DOWN_ARROW)
-        time.sleep(0.5)   
-    if steps(x) < 1.0:
-        kbd.send(Keycode.ALT, Keycode.UP_ARROW)
-        time.sleep(0.5)
 
-# #navigate Servers
-#     if steps(x) > 11.0:
-#         kbd.send(Keycode.LEFT_CONTROL, Keycode.ALT, Keycode.DOWN_ARROW)
-#         time.sleep(0.5)        
-#     if steps(x) < 9.0:
-#         kbd.send(Keycode.LEFT_CONTROL, Keycode.ALT, Keycode.UP_ARROW)
-#         time.sleep(0.5)   
+    if potVal > fakeVolume:
+        if not fakeVolume == 100 :
+            fakeVolume = fakeVolume+2
+            cc.press(ConsumerControlCode.VOLUME_INCREMENT)
+            # cc.release()
 
-#     if steps(x) > 19.0:
-#         kbd.send(Keycode.LEFT_CONTROL, Keycode.ALT, Keycode.DOWN_ARROW)
-#         time.sleep(0.5)   
-#     if steps(x) < 1.0:
-#         kbd.send(Keycode.LEFT_CONTROL, Keycode.ALT, Keycode.UP_ARROW)
-#         time.sleep(0.5)      
-
-#navigate chat
-    if steps(y) > 11.0:
-        mouse.move(wheel=-1)
-        time.sleep(0.1)
-    if steps(y) < 9.0:
-        mouse.move(wheel=1)
-        time.sleep(0.1)
-
-    if steps(y) > 19.0:
-        mouse.move(wheel=-1)
-        time.sleep(0.1)
-    if steps(y) < 1.0:
-        mouse.move(wheel=1)
-        time.sleep(0.1)
+    if potVal < fakeVolume:
+        if not fakeVolume == 0 :
+            fakeVolume = fakeVolume-2
+            cc.press(ConsumerControlCode.VOLUME_DECREMENT)
+            # cc.release()
+          
+    time.sleep(0.3)
+    kbd.release_all()   
+    cc.release()
